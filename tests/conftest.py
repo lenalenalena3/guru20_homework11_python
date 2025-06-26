2e024c555e26f3626567dc97e20c3b67621d104d
Elena Tinkalyuk <lenalenalena3@mail.ru>
2025-06-26 23:27:41 +0300

    корректировка

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,45 +5,21 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-@pytest.fixture(scope="function")
-def setup_browser():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-
-    browser.config.driver = driver
-    yield browser
-
-    browser.quit()
-
-
 @pytest.fixture(scope="function")
 def setup_browser():
     options = Options()
     options.page_load_strategy = 'eager'
     # Определяем, используется ли Selenoid
     use_selenoid = os.getenv('USE_SELENOID', 'false').lower() == 'true'
-
+    print(f" selenoid: {use_selenoid}")
     if use_selenoid:
         # Конфигурация для Selenoid
         selenoid_capabilities = {
             "browserName": "chrome",
-            "browserVersion": "100.0",
+            "browserVersion": "128.0",
             "selenoid:options": {
                 "enableVNC": True,
-                "enableVideo": True
+                "enableVideo": False
             }
         }
         options.capabilities.update(selenoid_capabilities)
@@ -52,6 +28,7 @@ def setup_browser():
             command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
             options=options
         )
+        browser.config.driver = driver
     else:
         # Локальная конфигурация
         driver_options = webdriver.ChromeOptions()
